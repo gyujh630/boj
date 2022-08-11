348cec33fb4099a319adab6f51d0d346ca7f7366
gyujh <rbwjdrbxo32@gmail.com>
2022-08-11 23:06:19 +0900

    Delete 2941.py

--- 2941.py
+++
@@ -1,9 +0,0 @@
-#크로아티아 알파벳
-
-s = input()
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-
-for i in range(len(croatia)):
-    if s.count(croatia[i]) > 0:
-        s = s.replace(croatia[i], '1')
-print(len(s))
