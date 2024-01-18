ee3a6be37638f4e53b1971e3b5bf9dbe39a51394
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-01-19 05:51:06 +0900

    new solve

--- boj_1926.py
+++ boj_1926.py
@@ -3,10 +3,9 @@
 import sys
 from collections import deque
 
-def bfs(map, x, y, visited):
+def bfs(map, x, y):
     queue = deque()
     queue.append((x, y))
-    visited[x][y] = True
     map[x][y] = 0
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -18,9 +17,8 @@ def bfs(map, x, y, visited):
         for i in range(4):
             nx, ny = cur[0] + dx[i], cur[1] + dy[i]
             if nx >= 0 and nx < n and ny >= 0 and ny < m:
-                if not visited[nx][ny] and map[nx][ny] == 1:
+                if map[nx][ny] == 1:
                     queue.append((nx, ny))
-                    visited[nx][ny] = True
                     map[nx][ny] = 0
     return count
 
@@ -29,13 +27,12 @@ n, m = map(int, sys.stdin.readline().split())
 map = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 max = 0
 count = 0
-visited = [[False] * m for _ in range(n)]
 
 for i in range(n):
     for j in range(m):
         if map[i][j] == 1:
             count += 1
-            temp = bfs(map, i, j, visited)
+            temp = bfs(map, i, j)
             if temp > max:
                 max = temp
 
--- boj_7576.py
+++ boj_7576.py
@@ -3,68 +3,57 @@
 import sys
 from collections import deque
 
-
-def bfs(map, riped_tomatoes, visited):
+def bfs(map, riped_tomatoes):
     queue = deque()
     count = 0
     temp = 0
     answer = 0
     total_change = 0
-    for riped_tomato in riped_tomatoes:
+    for riped_tomato in riped_tomatoes: # 시작점 여러개 큐에 넣기
         queue.append(riped_tomato)
         temp += 1
         total_change += 1
-        visited[riped_tomato[0]][riped_tomato[1]] = True
     dx, dy = [0, -1, 1, 0], [1, 0, 0, -1]
 
-    # temp = 2, count = 0 인 상태
     j = 1
     while queue:
         cur = queue.popleft()
         for i in range(4):
             nx, ny = cur[0] + dx[i], cur[1] + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
-                if not visited[nx][ny] and map[nx][ny] == 0:
+                if map[nx][ny] == 0:
                     queue.append((nx, ny))
-                    visited[nx][ny] = True
                     map[nx][ny] = 1
                     count += 1
                     total_change += 1
-        # for k in range(n):
-        #     print(map[k])
-        # print(j, "번째 큐 결과")
         if j == temp:
-            # print("count:", count, "이전 temp:", temp)
-            # print("사이클 종료", j, "번째 사이클")
             answer += 1
             temp = count
             count = 0
             j = 0
-        #     print("바뀐 temp:", temp)
-        # print(" ")
-        # print("answer:", answer)
-        # print('--------------------')
         j += 1
     return answer - 1, total_change
 
-
+# 입력 부분
 m, n = map(int, sys.stdin.readline().split())
 map = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 start_list = []
 wall_count = 0
 
+# 시작점(이미 익어 있는 토마토들) 탐색
 for i in range(n):
     for j in range(m):
         if map[i][j] == 1:
             start_list.append((i, j))
 
+# 벽(-1) 수 탐색
 for i in range(n):
     for j in range(m):
         if map[i][j] == -1:
             wall_count += 1
-visited = [[False] * m for _ in range(n)]
-answer, total = bfs(map, start_list, visited)
 
-if total != n*m-wall_count:
+answer, total = bfs(map, start_list)
+
+if total != n * m - wall_count: #불가 여부 판단
     print(-1)
 else: print(answer)
