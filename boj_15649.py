c6cabe634092c8c677e3fdeab4155daadb0874b1
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-01-24 23:25:26 +0900

    new solve

--- boj_15649.py
+++ boj_15649.py
@@ -2,17 +2,17 @@
 
 import sys
 
-def find_sequence(select, n, m):
+def find_sequence(select):
     if len(select) == m:
         print(*select)
     for i in range(1, n+1):
         if i not in select:
             select.append(i)
-            find_sequence(select, n, m)
+            find_sequence(select)
             select.pop()
 
     return
 
 n, m = map(int, sys.stdin.readline().split())
 
-find_sequence([], n, m)
+find_sequence([])
